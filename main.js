e6d9217b3ddface2e69169da84c09c796c35ae56
DevShayan <shayanahmed393@gmail.com>
2024-02-11 14:13:44 +0500

    Changes to be committed: 	modified:   index.html 	modified:   main.js

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log("OK");
+$(document).ready(main);
+
+function main() {
+	var myEmail = $("#my-email").text();
+	var myWhatsapp = $("#my-whatsapp").text();
+
+	// Email
+	$("#s3-email-card").click(function() {
+		window.location.href = `mailto:${myEmail}`;
+	});
+
+	// Whatsapp
+	$("#s3-whatsapp-card").click(function() {
+		window.location.href = `https://api.whatsapp.com/send?phone${myWhatsapp}&text=%20msg`;
+	});
+}
