6c9b4fc85075406d3efa986a9f5170614bcb51d5
DevShayan <shayanahmed393@gmail.com>
2024-02-27 19:19:24 +0500

    Changes to be committed: 	modified:   assets/images/profile.jpg 	modified:   index.html 	modified:   main.css 	modified:   main.js

--- main.js
+++ main.js
@@ -4,13 +4,25 @@ function main() {
 	var myEmail = $("#my-email").text();
 	var myWhatsapp = $("#my-whatsapp").text();
 
-	// Email
 	$("#s3-email-card").click(function() {
-		window.location.href = `mailto:${myEmail}`;
+		window.open(`mailto:${myEmail}`, "_blank");
 	});
 
-	// Whatsapp
 	$("#s3-whatsapp-card").click(function() {
-		window.location.href = `https://api.whatsapp.com/send?phone${myWhatsapp}&text=%20msg`;
+		window.open(`https://api.whatsapp.com/send?phone=${myWhatsapp}&text=%20`, "_blank");
+	});
+
+	$("#s3-send-msg-btn").click(function() {
+		var name = "PORTFOLIO MESSAGE: "+$("#s3-contact-name-input").val();
+		var message = $("#s3-msg-text").val();
+		window.open(`mailto:${myEmail}?subject=${name}&body=${message}`, "_blank");
+	});
+
+	$("#s4-gpa-calc-card").click(function() {
+		window.open("https://devshayan.github.io/gpa_calculator/", "_blank");
+	});
+
+	$("#s4-snake-card").click(function() {
+		window.open("https://devshayan.github.io/snake_game/", "_blank");
 	});
 }
