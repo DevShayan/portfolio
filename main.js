114ade4dee3d1c9f3ee093181ce79350964989aa
DevShayan <shayanahmed393@gmail.com>
2024-06-11 04:45:43 -0400

    Changes to be committed: 	modified:   assets/images/proj1/assignments.png 	deleted:    assets/images/proj1/attendance_class.png 	deleted:    assets/images/proj1/attendance_total.png 	new file:   assets/images/proj1/attn_class.png 	new file:   assets/images/proj1/attn_total.png 	modified:   assets/images/proj1/dashboard.png 	new file:   assets/images/proj1/grades_sem.png 	deleted:    assets/images/proj1/grades_semester.png 	modified:   assets/images/proj1/grades_total.png 	modified:   assets/images/proj1/login.png 	modified:   assets/images/proj1/register.png 	modified:   assets/images/proj1/schedule.png 	new file:   assets/images/proj2/1.png 	new file:   assets/images/proj2/2.png 	new file:   assets/images/proj2/3.png 	new file:   assets/images/proj2/4.png 	new file:   assets/images/proj2/5.png 	modified:   index.html 	modified:   main.css 	modified:   main.js

--- main.js
+++ main.js
@@ -1,3 +1,39 @@
+var proj1Images = [
+	"assets/images/proj1/login.png",
+	"assets/images/proj1/register.png",
+	"assets/images/proj1/dashboard.png",
+	"assets/images/proj1/assignments.png",
+	"assets/images/proj1/attn_total.png",
+	"assets/images/proj1/attn_class.png",
+	"assets/images/proj1/grades_total.png",
+	"assets/images/proj1/grades_sem.png",
+	"assets/images/proj1/schedule.png"
+];
+var index1 = 0;
+
+var proj2Images = [
+	"assets/images/proj2/1.png",
+	"assets/images/proj2/2.png",
+	"assets/images/proj2/3.png",
+	"assets/images/proj2/4.png",
+	"assets/images/proj2/5.png"
+];
+var index2 = 0;
+
+var proj3Images = [
+	"assets/images/proj1/login.png",
+	"assets/images/proj1/register.png",
+	"assets/images/proj1/dashboard.png",
+	"assets/images/proj1/assignments.png",
+	"assets/images/proj1/attn_total.png",
+	"assets/images/proj1/attn_class.png",
+	"assets/images/proj1/grades_total.png",
+	"assets/images/proj1/grades_sem.png",
+	"assets/images/proj1/schedule.png"
+];
+var index3 = 0;
+
+
 $(document).ready(main);
 
 
@@ -37,13 +73,58 @@ function section3() {
 
 
 function section4() {
-	$("#s4-gpa-calc-card").click(function() {
-		window.open("https://devshayan.github.io/gpa_calculator/", "_blank");
+	// initialize
+	$("#proj1-img").attr("src", proj1Images[index1]);
+	$("#proj2-img").attr("src", proj2Images[index2]);
+	$("#proj3-img").attr("src", proj3Images[index3]);
+
+	var timeMs = 5000;
+
+	var proj1Timer = setInterval(changeProj1Images, timeMs);
+	var proj2Timer = setInterval(changeProj2Images, timeMs);
+	var proj3Timer = setInterval(changeProj3Images, timeMs);
+
+	/////////////
+
+	$("#proj1-img").click(function() {
+		clearInterval(proj1Timer);
+		proj1Timer = setInterval(changeProj1Images, timeMs);
+
+		(index1+1 >= proj1Images.length) ? index1 = 0 : index1++;
+		$("#proj1-img").attr("src", proj1Images[index1]);
 	});
 
-	$("#s4-snake-card").click(function() {
-		window.open("https://devshayan.github.io/snake_game/", "_blank");
+	$("#proj2-img").click(function() {
+		clearInterval(proj2Timer);
+		proj2Timer = setInterval(changeProj2Images, timeMs);
+
+		(index2+1 >= proj2Images.length) ? index2 = 0 : index2++;
+		$("#proj2-img").attr("src", proj2Images[index2]);
 	});
+
+	$("#proj3-img").click(function() {
+		clearInterval(proj3Timer);
+		proj3Timer = setInterval(changeProj3Images, timeMs);
+
+		(index3+1 >= proj3Images.length) ? index3 = 0 : index3++;
+		$("#proj3-img").attr("src", proj3Images[index3]);
+	});
+}
+
+
+function changeProj1Images() {
+	(index1+1 >= proj1Images.length) ? index1 = 0 : index1++;
+	$("#proj1-img").attr("src", proj1Images[index1]);
+}
+
+function changeProj2Images() {
+	(index2+1 >= proj2Images.length) ? index2 = 0 : index2++;
+	$("#proj2-img").attr("src", proj2Images[index2]);
+}
+
+function changeProj3Images() {
+	(index3+1 >= proj3Images.length) ? index3 = 0 : index3++;
+	$("#proj3-img").attr("src", proj3Images[index3]);
 }
 
 
